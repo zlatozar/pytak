35f4780575af78dc436806404acec8d464321041
Zlatozar Zhelyazkov <zlatozar@gmail.com>
2014-07-09 11:59:00 +0300

    Rename argument add comments
    Signed-off-by: Zlatozar Zhelyazkov <zlatozar@gmail.com>

--- pytak/predicates.py
+++ pytak/predicates.py
@@ -15,20 +15,20 @@ import logging
 
 log = logging.getLogger(__name__)
 
-def created_before_hours(id, before=2):
-    "Checks given id mask if post is created before given hour"
+def created_before_hours(id_reg, before=2):
+    "Checks given id_reg mask if post is created before given hour"
 
     hours_before = datetime.datetime.now() - datetime.timedelta(hours=before)
 
     def created_before_hours_p(clazz, idx):
-        """Compose idx and id to form key from stack data and check if date is bigger that 'before'"""
-        creation_date = datetime.datetime.fromtimestamp(float(clazz[__replace_star(id, idx)]) / 1000.0)
+        """Compose idx and id_reg to form key from stack data and check if date is bigger that 'before'"""
+        creation_date = datetime.datetime.fromtimestamp(float(clazz[__replace_star(id_reg, idx)]) / 1000.0)
 
         if creation_date < hours_before:
             return True
 
         else:
-            log.error("Predicate '%s' fails for %s:%s" % ("created_before_hours_p", __replace_star(id, idx), creation_date))
+            log.debug("Predicate '%s' fails for %s:%s" % ("created_before_hours_p", __replace_star(id_reg, idx), creation_date))
 
         return False
 
@@ -39,5 +39,6 @@ def created_before_hours(id, before=2):
 # ___________________________________________________________
 #                                           Helper functions
 
-def __replace_star(id, idx):
-    return id.replace('*', idx)
+def __replace_star(id_reg, idx):
+    """Replace stars in id_reg with an index value"""
+    return id_reg.replace('*', idx)
